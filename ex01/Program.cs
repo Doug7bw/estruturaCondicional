4bf9853d9f3a9367a7e6b536a64d950db7a84249
Doug7bw <110914284+Doug7bw@users.noreply.github.com>
2023-01-06 16:27:11 -0300

    Add files via upload

---
+++ ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex01;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+        
+        Console.WriteLine("Digite um número inteiro:");
+        number = int.Parse(Console.ReadLine());
+
+        if(number > 0){
+            Console.WriteLine("NAO NEGATIVO");
+        }
+        else{
+            Console.WriteLine("NEGATIVO");
+        };
+    }
+}
