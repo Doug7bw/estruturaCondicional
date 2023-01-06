452192d328f986db963c5e6fc83991f05ea7dd13
Doug7bw <110914284+Doug7bw@users.noreply.github.com>
2023-01-06 16:30:07 -0300

    Add files via upload

---
+++ ex07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex07;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x, y;
+
+        Console.WriteLine("Digite o valor de X:");
+        x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Ditite o valor de Y");
+        y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        if(x == 0 && y == 0){
+            Console.WriteLine("Origem");
+        }
+        else if(x >= 0 && y >= 0){
+            Console.WriteLine("Q1");
+        }
+        else if(x >= 0 && y <= 0){
+            Console.WriteLine("Q4");
+        }
+        else if(x <= 0 && y >= 0){
+            Console.WriteLine("Q2");
+        }
+        else if(x <= 0 && y <= 0){
+            Console.WriteLine("Q3");
+        };
+    }
+}
