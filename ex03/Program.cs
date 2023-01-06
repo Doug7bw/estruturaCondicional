b16b57570b07ec94420d585ca5b64a5532f2f445
Doug7bw <110914284+Doug7bw@users.noreply.github.com>
2023-01-06 16:28:15 -0300

    Add files via upload

---
+++ ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+        Console.WriteLine("Digite um número inteiro para 'A':");
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite um número inteiro para 'B'");
+        b = int.Parse(Console.ReadLine());
+
+        if((a % b == 0 || b % a == 0)){
+            Console.WriteLine("São Multiplos");
+        }
+        else{
+            Console.WriteLine("Não são Multiplos");
+        }
+    }
+}
