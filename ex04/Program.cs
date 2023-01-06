54269e42859769ce6a57cacb50c55e238c6ddcc6
Doug7bw <110914284+Doug7bw@users.noreply.github.com>
2023-01-06 16:29:25 -0300

    Add files via upload

---
+++ ex04/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ex04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int horaInicio, horaFim, duracao = 0;
+
+        Console.WriteLine("Digite a hora que o jogo começou:");
+        horaInicio = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a hora que o jogo terminou:");
+        horaFim = int.Parse(Console.ReadLine());
+
+        if(horaInicio > horaFim){
+            duracao = 24 - horaInicio + horaFim;
+        }
+        else{
+            duracao = 24 + horaInicio - horaFim;
+        };
+
+        if(duracao >+ 1 && duracao <= 24){
+            Console.WriteLine($"O jogo durou {duracao} horas");
+        }
+        else{
+            Console.WriteLine("A partida precisa ter uma duração mínima de 1 hora e duração máxima de 24 horas!");
+        };      
+    }
+}
---
+++ ex05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex05;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int codigo, quantidade;
+        string item = "";
+        double precoItem = 0.0, precoTotal = 0.0;
+
+        Console.WriteLine("Digite o código do item que deseja levar (De 1 a 5):");
+        codigo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de itens que vai levar:");
+        quantidade = int.Parse(Console.ReadLine());
+
+        if(codigo >= 1 && codigo <= 5){
+            if(codigo == 1){
+                precoTotal = 4.00 * quantidade;
+                item = "Cachorro quente";
+            }
+            else if(codigo == 2){
+                precoTotal = 4.50 * quantidade;
+                item = "X-Salada";
+            }
+            else if(codigo == 3){
+                precoTotal = 5.00 * quantidade;
+                item = "X-Bacon";
+            }
+            else if(codigo == 4){
+                precoTotal = 2.00 * quantidade;
+                item = "Torrada simples";
+            }
+            else if(codigo == 5){
+                precoTotal = 1.50 * quantidade;
+                item = "Refrigerante";
+            };
+            Console.WriteLine($"Comprando {quantidade} {item}(s), o total a pagar é: R$ {precoTotal.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        else{
+            Console.WriteLine($"Não existe um item com o código {codigo}");
+        };
+    }
+}
